a4c4171922bbf31bb54dc541bda679917ebf771e
matteo granata <matteogranata92@gmail.com>
2025-01-30 18:37:57 +0100

    add tailwindCSS plugin for :not-emplty, :not-empty-not-first-child ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from "tailwindcss/plugin";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -15,5 +16,35 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant, e }) {
+      // Variante per :not(:empty)
+      addVariant("not-empty", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`not-empty${separator}${className}`)}:not(:empty)`;
+        });
+      });
+
+      // Variante per :not(:first-child)
+      addVariant("not-first-child", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(
+            `not-first-child${separator}${className}`
+          )}:not(:first-child)`;
+        });
+      });
+
+      // Variante combinata :not(:empty):not(:first-child)
+      addVariant(
+        "not-empty-not-first-child",
+        ({ modifySelectors, separator }) => {
+          modifySelectors(({ className }) => {
+            return `.${e(
+              `not-empty-not-first-child${separator}${className}`
+            )}:not(:empty):not(:first-child)`;
+          });
+        }
+      );
+    }),
+  ],
 };
