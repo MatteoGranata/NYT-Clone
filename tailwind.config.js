fd663e74e18fefda91db65787dd506fee7f7707c
matteo granata <matteogranata92@gmail.com>
2025-01-30 17:41:18 +0100

    added theme extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2lg": "1070px",
+      },
+      flex: {
+        2: "0 0 91px",
+        3: " 1 75%;",
+      },
+      padding: {
+        ny: "0px 0px 100%",
+      },
+    },
   },
   plugins: [],
-}
+};
