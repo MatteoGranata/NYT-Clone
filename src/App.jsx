e7d2743114f9216d3420794127ddde2f3df0bafc
matteo granata <matteogranata92@gmail.com>
2025-01-30 18:42:57 +0100

    add NavBar and Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import FooterPage from "./components/FooterPage";
+import Navbar from "./components/navBar/NavBar";
 import ArticleList from "./pages/ArticlePages";
 import TopArticleList from "./pages/TopArticlePage";
 
@@ -5,6 +7,7 @@ function App() {
   return (
     <div className="justify-center flex">
       <div className="items-center flex flex-col m-0 max-w-[1200px] w-full">
+        <Navbar />
         <div className="grid 2lg:grid-cols-3 grid-cols-1 gap-8 p-4 2lg:p-0">
           <div className=" 2lg:col-span-2 col-span-1">
             <ArticleList />
@@ -13,6 +16,7 @@ function App() {
             <TopArticleList />
           </div>
         </div>
+        <FooterPage />
       </div>
     </div>
   );
