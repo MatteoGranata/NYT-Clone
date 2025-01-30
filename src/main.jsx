92b73449533e018c07a2660df18882600ddbd676
matteo granata <matteogranata92@gmail.com>
2025-01-30 18:41:27 +0100

    fixed import

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import store from "./redux/store.js";
 
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
 import SectionPage from "./pages/SectionPage.jsx";
 import SearchPage from "./pages/SearchPage.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
