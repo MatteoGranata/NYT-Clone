1adcd972b72aea390b002e3dba0f6b47fe2a1459
matteo granata <matteogranata92@gmail.com>
2025-01-30 18:07:43 +0100

    configurated react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,29 @@ import App from "./App.jsx";
 import store from "./redux/store.js";
 
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SectionPage from "./pages/SectionPage.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/section/:sectionID",
+    element: <SectionPage />,
+  },
+  {
+    path: "/search/:searchID",
+    element: <SearchPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
