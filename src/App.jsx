a24c620fa48b39d608feb8b35a5e2aeaa7216408
matteo granata <matteogranata92@gmail.com>
2025-02-03 20:56:27 +0100

    fixed padding

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="justify-center flex">
       <div className="items-center flex flex-col m-0 max-w-[1200px] w-full">
         <Navbar />
-        <div className="grid 2lg:grid-cols-3 grid-cols-1 gap-8 p-4 2lg:p-0">
+        <div className="grid 2lg:grid-cols-3 grid-cols-1 gap-8 px-4 xl:p-0">
           <div className=" 2lg:col-span-2 col-span-1">
             <ArticleList />
           </div>
